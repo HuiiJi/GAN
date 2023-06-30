f064a2da6b3e9b10f82d1a9d871bafd68c5c6fe5
Huii Ji <72867414+HuiiJi@users.noreply.github.com>
2023-06-30 23:30:00 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 from GAN_ import Config, Gnet
+import torchvision
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu' #定义模型在GPU的cuda下跑,没有cuda则在CPU慢慢跑
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #定义模型在GPU的cuda下跑,没有cuda则在CPU慢慢跑
 
 opt = Config() #实例化Config
 g_net = Gnet(opt) #实例化生成模型
@@ -18,4 +19,4 @@ g_net.load_state_dict(ckpt_dict) #将字典化的weights加载进生成模型，
 input_noise = torch.randn(opt.batch_size, opt.noise_dim, 1, 1).to(device) #输入定义为正态分布的噪声
 out_img = g_net(input_noise) #噪声进入生成模型，推理得到image
 
-torchvision.utils.save_image(out_image.data, "%s/inference.png" % (opt.result_save_path)), normalize=True) #保存image
+torchvision.utils.save_image(out_image.data[0], "%s/inference.png" % (opt.result_save_path)), normalize=True) #保存image
