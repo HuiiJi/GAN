7be7ca0837ec1ced5c859f6c45db6d4bbc28c13a
Huii Ji <72867414+HuiiJi@users.noreply.github.com>
2023-06-30 23:52:36 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -11,12 +11,12 @@ g_net = Gnet(opt) #实例化生成模型
 g_net = g_net.to(device)
 
 
-ckpt = 'snapshots/gnet.pth' #生成网络训练好后weights的地址
+ckpt = 'snapshots/gnet.pth' #生成网络训练好后weights的地址,也可以ckpt=opt.g_net_path
 ckpt_dict = torch.load(ckpt) #将weights的字典加载出来
 g_net.load_state_dict(ckpt_dict) #将字典化的weights加载进生成模型，即该模型填充了训练好的weights
-
+g_net.eval()
 
 input_noise = torch.randn(opt.batch_size, opt.noise_dim, 1, 1).to(device) #输入定义为正态分布的噪声
 out_img = g_net(input_noise) #噪声进入生成模型，推理得到image
 
-torchvision.utils.save_image(out_image.data[0], "%s/inference.png" % (opt.result_save_path)), normalize=True) #保存image
+torchvision.utils.save_image(out_img.data[0], "%s/inference.png" % (opt.result_save_path), normalize=True) #保存image
